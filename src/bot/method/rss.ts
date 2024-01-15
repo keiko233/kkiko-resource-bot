0e8327fdb9b48e62ee14c456a8965e4138c9f2ad
keiko233 <i@elaina.moe>
2024-01-15 14:35:22 +0800

    fix: use tmdb search name

--- src/bot/method/rss.ts
+++ src/bot/method/rss.ts
@@ -1,10 +1,17 @@
-import { array2text, isLink, rssRegex } from "@/utils";
+import {
+  Extract,
+  array2text,
+  downloadFileToBuffer,
+  isLink,
+  rssRegex,
+} from "@/utils";
 import { CommandContext } from "../utils";
 import { ofetch } from "ofetch";
 import { ElementCompact, xml2js } from "xml-js";
 import { Markup } from "telegraf";
 import { bot } from "../common";
 import { createRss } from "@/model";
+import parseTorrent from "parse-torrent";
 
 export const rss = async (ctx: CommandContext) => {
   const randomId = Math.random();
@@ -39,8 +46,30 @@ export const rss = async (ctx: CommandContext) => {
 
   const rss = rssRegex(result.rss);
 
+  const buffer = await downloadFileToBuffer(rss[0].url);
+
+  const torrentInfo = parseTorrent(buffer);
+
+  const extract = new Extract();
+
+  await extract.regex(torrentInfo.name as string);
+
   ctx.reply(
-    array2text(["RSS解析结果: ", ...rss.map((element) => element.title)]),
+    array2text([
+      "解析结果: ",
+      `标题: ${extract.regexName}`,
+      `季度: ${extract.season}`,
+      `集数: ${extract.episode}`,
+      "",
+      "搜索结果: ",
+      `名称: ${extract.tmdbDetails.name}`,
+      `季度: ${extract.tmdbSeasonDetails.name}`,
+      `集数信息: ${extract.tmdbEpisodeDetails.name}`,
+      `TMDB ID: ${extract.tmdbDetails.id}`,
+      "",
+      "RSS 资源解析结果: ",
+      ...rss.map((element) => element.title),
+    ]),
     Markup.inlineKeyboard([
       Markup.button.callback(
         "确认无误，添加订阅",
@@ -55,7 +84,7 @@ export const rss = async (ctx: CommandContext) => {
     const result = await createRss({
       requestUser: first_name,
       requestUserId: id,
-      name: "CustomRss",
+      name: extract.tmdbDetails.name,
       url,
       regex: JSON.stringify(rss),
     });
