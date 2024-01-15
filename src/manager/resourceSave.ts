fa310775347d2e4eee16ad690e94736f9faeedac
keiko233 <i@elaina.moe>
2024-01-15 11:06:51 +0800

    fix: data stagnation when not being able to obtain data

--- src/manager/resourceSave.ts
+++ src/manager/resourceSave.ts
@@ -48,48 +48,56 @@ export const resourceSave = async (id: number, torrent: Buffer) => {
   });
 
   const showStatus = async (lastProgress?: string) => {
-    const torrent = await qbittorrent.getTorrent(task.hash);
+    try {
+      const torrent = await qbittorrent.getTorrent(task.hash);
 
-    const progress = (torrent.progress * 100).toFixed(2);
+      const progress = (torrent.progress * 100).toFixed(2);
 
-    if (torrent.progress < 1) {
-      if (progress != lastProgress) {
-        callback(
-          `下载进度: ${progress}% (${format.size(
-            torrent.totalDownloaded
-          )} / ${format.size(torrent.totalSize)})`
-        );
+      if (torrent.progress < 1) {
+        if (progress != lastProgress) {
+          callback(
+            `下载进度: ${progress}% (${format.size(
+              torrent.totalDownloaded
+            )} / ${format.size(torrent.totalSize)})`
+          );
 
-        setTimeout(async () => {
-          await showStatus(progress);
-        }, 3000);
+          setTimeout(async () => {
+            await showStatus(progress);
+          }, 3000);
+        }
+      } else {
+        const lastTask = await updateTaskById(task.id, {
+          message: `下载完成 (${format.size(torrent.totalSize)})`,
+          status: "successfully-downloaded",
+        });
+
+        await callback("准备上传视频并重命名");
+
+        const rename = new Rename(lastTask);
+        await rename.run();
+
+        await callback("正在生成元信息");
+
+        const nfo = new Nfo();
+        await nfo.run(lastTask, rename.tvName);
+
+        await qbittorrent.removeTorrent(lastTask.hash, true);
+
+        await uploadTmpDir(`tmp/task-${lastTask.id}`);
+
+        deleteFolder(`tmp/task-${lastTask.id}`);
+
+        await updateTaskById(task.id, {
+          status: "complated",
+          message: "处理完成",
+        });
       }
-    } else {
-      const lastTask = await updateTaskById(task.id, {
-        message: `下载完成 (${format.size(torrent.totalSize)})`,
-        status: "successfully-downloaded",
-      });
+    } catch (err) {
+      log(err);
 
-      await callback("准备上传视频并重命名");
-
-      const rename = new Rename(lastTask);
-      await rename.run();
-
-      await callback("正在生成元信息");
-
-      const nfo = new Nfo();
-      await nfo.run(lastTask, rename.tvName);
-
-      await qbittorrent.removeTorrent(lastTask.hash, true);
-
-      await uploadTmpDir(`tmp/task-${lastTask.id}`);
-
-      deleteFolder(`tmp/task-${lastTask.id}`);
-
-      await updateTaskById(task.id, {
-        status: "complated",
-        message: "处理完成",
-      });
+      setTimeout(async () => {
+        await showStatus();
+      }, 3000);
     }
   };
 
